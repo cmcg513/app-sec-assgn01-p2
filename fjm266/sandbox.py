be94a3741f106cf3d2b55ae856cad09f3c4671de
Casey McGinley <cmcg513@gmail.com>
2015-09-22 15:35:56 -0400

    Can't find a good exploit for fjm266's sandbox

---
+++ fjm266/sandbox.py
@@ -0,0 +1,17 @@
+import sys
+
+if len(sys.argv)>2:
+    code = open(sys.argv[1],'rb').read()
+    data = open(sys.argv[2], 'rb').read()
+elif len(sys.argv)==2:
+    code = open(sys.argv[1],'rb').read()
+    data = open('data.in', 'rb').read()
+elif len(sys.argv)<2:
+    code = open('code.in','rb').read()
+    data = open('data.in', 'rb').read()
+
+
+namespace = {}
+namespace['__builtins__'] = {'True':True, 'False':False, 'None':None, 'data':data,'dir':dir,'locals':locals}
+
+exec code in namespace
